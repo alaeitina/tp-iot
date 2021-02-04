898c2f5d671cf973687f6c23e0b212389dc4e546
Mimoun MOHAMED <mimoun.mohamed@centrale-marseille.fr>
2021-02-04 11:29:37 +0100

    Debuggage terminee

--- Temperature/humidite.py
+++ Temperature/humidite.py
@@ -1,4 +1,4 @@
-from .temperature import get_temperature
+#from .temperature import get_temperature
 
 values = [
     [0.76, 0.73, 0.70, 0.67, 0.63, 0.60, 0.56],
@@ -13,6 +13,23 @@ values = [
 
 abscisses = [10, 15, 20, 25, 30, 35, 40]  # Liste des températures associées
 ordonnees = [20 + 10*k for k in range(8)]  # Liste des pourcentages d'humidité associé
+
+def get_temperature():
+    """
+    Renvoie la température mesurée par le capteur
+    """
+    i2c = I2C(0, pins=('P9','P10'))              # Instancie la connexion
+    i2c.init(I2C.MASTER, baudrate=20000)         # Met la Pycom en position de maître pour la liaison avec le capteur
+    val = i2c.readfrom(72, 2)                    # Effectue la lecture du capteur 
+    val_str = str(val)                           # Converti le byte en chaine de caractère
+    hex_val = val_str[4:6] + val_str[8:10]       # Extrait les 4 caractères qui donnent la mesure de température en hexa
+    bin_val = bin(int(hex_val, 16))              # Convertion hexadémicale -> binaire
+
+    temp_bin = bin_val[2:-4]                     # Découpe le LSB de la température en binaire
+    temp_int = int(temp_bin, 2)                  # Convertion Binaire -> Décimale
+    temp_degree = temp_int * 0.0625              # Mise à l'échelle de la température
+    i2c.deinit()                                 # Etient le périphérique
+    return temp_degree
 
 def get_indices_of_neighbour(value, liste):
     """
@@ -63,8 +80,8 @@ def get_humidite(temperature, tension):
     return interpolation(tension, newcol[indinftens], newcol[indsuptens], ordonnees[indinftens], ordonnees[indsuptens])
 
 
-temperature = 12 #get_temperature()
+temperature = get_temperature()
 
-tension = 1.92 #Récupérer ici la valeur de la tension
+tension = 1.92 # Récupérer ici la valeur de la tension
 
 print(get_humidite(temperature, tension))
