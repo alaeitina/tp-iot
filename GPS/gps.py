91cde17a12d15bbfeedd3c114a266aadd3e234c9
Tina <tina.alaei@centrale-marseille.fr>
2021-02-18 11:29:05 +0100

    read trame

--- GPS/gps.py
+++ GPS/gps.py
@@ -1,10 +1,34 @@
 from machine import I2C
+import time
 
-i2c = I2C(0, pins=('P22','P21'))
-print(i2c.scan())
+def read_byte():
+    i2c = I2C(0, pins=('P22','P21'))
+    a16 = i2c.readfrom(16, 255)
+    return a16
 
-a8 = i2c.readfrom(8, 255)
-a16 = i2c.readfrom(16, 255)
-a30 = i2c.readfrom(30, 255)
+def read_msg():
+    a = read_byte()
+    astr = str(a)[2:-1]
 
-print(str(a16))
+    liste = astr.split('\\r\\n$')[1:-1]
+    
+    return liste
+
+def loop_read_trame_GPGGA():
+    while True:
+        msgs = read_msg()
+        gpgga = [msg for msg in msgs if 'GPGGA' in msg]
+        print(gpgga)
+        if gpgga:
+            paramsplit = gpgga[0].split(',')
+            params = {}
+            params['time'] = paramsplit[1][:2]+':'+paramsplit[1][2:4]+':'+paramsplit[1][4:6]
+            if paramsplit[2] != '':
+                params['latitude'] = float(paramsplit[2][:2])+float(paramsplit[2][2:])/60 * ((-1)*(paramsplit[3] == 'S'))
+                params['longitude'] = float(paramsplit[5][:2])+float(paramsplit[5][2:])/60 * ((-1)*(paramsplit[3] == 'W'))
+                params['altitude'] = float(paramsplit[9])
+            print(params)
+        time.sleep(10)
+
+
+loop_read_trame_GPGGA()
